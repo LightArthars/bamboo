481e5eea1a150301e9990799f4f87952cfdde87c
Arthars <Arthars@et.com>
2020-09-01 21:19:09 +0800

    作业完成

--- homework.java
+++ homework.java
@@ -1,5 +1,6 @@
 public class homework{
     public static void main(String[] args){
-            System.out.println("家庭作业");
+            System.out.println("家庭作业写完了吗");
+	    System.out.println("写完啦");
     }
 }
